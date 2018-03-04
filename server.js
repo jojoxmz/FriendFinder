8865c6db40eb60bccf1dae9ac212628b1b7a32dc
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-03-04 16:12:03 -0700

    added all working functions

--- server.js
+++ server.js
@@ -4,21 +4,18 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-
-// app.get("/", function(req, res){
-// 	res.send("DOHHHHHH!");
-// });
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
+app.use(bodyParser.text());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-
-
-
